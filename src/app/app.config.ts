ed42d19266d5db27cdf61f21fac9205d1e76073a
SSebasthian <ssebasthian.perez@gmail.com>
2024-02-26 20:20:47 -0500

    Subiendo Prueba Login con FIREBASE

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideAnimationsAsync(), 
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(
+      {
+        "projectId":"angularsesion",
+        "appId":"1:588987184938:web:04fe3b72a26a0384c29053",
+        "storageBucket":"angularsesion.appspot.com",
+        "apiKey":"AIzaSyD8SWYCWlhVjFN2Am9cKMGLmDPgkwF_AX4",
+        "authDomain":"angularsesion.firebaseapp.com",
+        "messagingSenderId":"588987184938"
+      }))), 
+      importProvidersFrom(provideAuth(() => getAuth()))]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegistroComponent } from './pages/auth/registro/registro.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  {path:'home',
+    component:HomeComponent
+  },
+  {path:'login',
+    component:LoginComponent
+  },
+  {path:'registro',
+    component:RegistroComponent
+  },
+  {path:'**',
+    component:HomeComponent
+  }
+
+];
